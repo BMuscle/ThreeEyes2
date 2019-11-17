3c142c5244d113a22979e6147039fc206af6e676
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-11-17 20:26:11 +0900

    ロードエフェクト名前変更

--- ThreeEyes/LoadEffect.h
+++ ThreeEyes/LoadEffect.h
@@ -9,8 +9,8 @@ void initLoadEffect(void);
 void deleteLoadEffect(void);
 
 void onLoadFlag(void);
-BOOL getLoadFlag(void);
+int getLoadFlag(void);
 BOOL isLoadEnd(void);
 BOOL isStart(void);
-BOOL LoadEffect_Update(void);
+void LoadEffect_Update(void);
 void LoadEffect_Draw(void);
