f0c5ca4367f0bef285bf87e69e1504f17ec9d3a7
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-11-19 11:03:31 +0900

    ストップウォッチとターン表示追加

--- ThreeEyes/Lottery.cpp
+++ ThreeEyes/Lottery.cpp
@@ -2,7 +2,10 @@
 
 
 static Sprite lottery[2];
+static MyString message;
 static BOOL isEnd;
+static BOOL isRun;//計算処理が動いてるかどうか
+static int font;
 static int frameCount;
 static int countLimit;
 static int currentTurn;//ゲームで決まったターン
@@ -13,6 +16,7 @@ static int limitTimes;//ぐるぐる回る回数
 
 void initLottery(int turn) {
 	isEnd = FALSE;
+	isRun = FALSE;
 	currentTurn = turn - 1;//ゲームのターンは１始まりのため１引く	
 	frameCount = 0;
 	countLimit = 10;
@@ -20,8 +24,16 @@ void initLottery(int turn) {
 	times = 0;
 	limitTimes = GetRand(5) + 5;
 
-	lottery[0] = initSprite("images/senkou.png", 264, 299);
-	lottery[1] = initSprite("images/koukou.png", 264, 299);
+	lottery[0] = initSprite("images/senkou1.png", 274, 318);
+	lottery[1] = initSprite("images/koukou1.png", 274, 318);
+	font = CreateFontToHandle("ゴシック", 30, 6, DX_FONTTYPE_ANTIALIASING);
+	if (turn == 1) {
+		message = initMyString("あなたは先攻です。",0xFFFFFF,font, WINDOW_WIDTH/2, 30);
+	}
+	else {
+		message = initMyString("あなたは後攻です。", 0xFFFFFF, font, WINDOW_WIDTH / 2, 30);
+	}
+	message.alpha = 0;
 }
 void deleteLottery() {
 	deleteSprite(&lottery[0]);
@@ -29,7 +41,8 @@ void deleteLottery() {
 }
 
 void updateLottery() {
-	if (isEndLottery() == FALSE) {
+	if (isEnd == FALSE) {
+		isRun = TRUE;
 		frameCount++;
 		if (frameCount % countLimit == 0) {
 			frameCount = 0;
@@ -38,16 +51,23 @@ void updateLottery() {
 			times++;
 			if (limitTimes < times && nowTurn != currentTurn) {
 				isEnd = TRUE;
+				isRun = FALSE;
+				message.alpha = 255;
 			}
+		}
+	}
+	else {
+		message.alpha -= 2;
+		if (message.alpha < 0) {
+			message.alpha = 0;
 		}
 	}
 }
 void drawLottery() {
-	if (isEndLottery() == FALSE) {
+	if (isRun) {
 		drawAtSprite(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, &lottery[nowTurn], TRUE);
 	}
-}
-
-BOOL isEndLottery() {
-	return isEnd;
+	else {
+		mydrawAtString(&message);
+	}
 }
--- ThreeEyes/Lottery.h
+++ ThreeEyes/Lottery.h
@@ -2,9 +2,9 @@
 #include "DxLib.h"
 #include "Sprite.h"
 #include "MyWindow.h"
+#include "MyDrawString.h"
 
 void initLottery(int turn);
 void deleteLottery(void);
 void updateLottery(void);
 void drawLottery(void);
-BOOL isEndLottery(void);
