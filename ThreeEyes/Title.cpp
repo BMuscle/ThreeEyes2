0d2e40a535eed60bf8b9734b8624743938bff7be
日高 広夢 <chiro3330@i.softbank.jp>
2019-11-22 00:47:08 +0900

    選択画像の置き換え

--- ThreeEyes/Title.cpp
+++ ThreeEyes/Title.cpp
@@ -5,6 +5,9 @@
 #include"MassageBox.h"
 #include "LoadEffect.h"
 
+#define BUTTON_W 300				//画像の幅指定用マクロ
+#define BUTTON_H 60					//画像の高さ指定のマクロ
+
 static Sprite backSprite;
 static int Title;					//タイトル用のハンドル
 static int title_enter_SE, title_select_SE;		//音用ハンドル
@@ -13,31 +16,32 @@ static int Font00;					//フォント指定用のハンドル
 static int startSelect, endSelect;		//SE管理用の変数
 static int title_bgm;					//bgm用のハンドル
 
-static MassageBox startGame;
-static MassageBox endGame;
+static Sprite startButton[1];			//画像のハンドル
+static Sprite endButton[1];
+
+
 
 void Title_Initialize() {
 	Font00 = CreateFontToHandle("ゴシック", 15, 6, DX_FONTTYPE_ANTIALIASING);
-	backSprite = initSprite("images/titleback.png", 640, 480);
+	backSprite = initSprite("images/titleback3.png", 640, 480);
 	Title = LoadGraph("images/title.png");
 	title_enter_SE = LoadSoundMem("musics/enter_ou.wav");
 	title_select_SE = LoadSoundMem("musics/select_or_enter.wav");
 	title_bgm = LoadSoundMem("musics/bgm_title.wav");
-	ChangeVolumeSoundMem( 255 * 40 / 100 ,title_bgm);
-	PlaySoundMem(title_bgm,DX_PLAYTYPE_LOOP);
+	ChangeVolumeSoundMem(255 * 40 / 100, title_bgm);
+	PlaySoundMem(title_bgm, DX_PLAYTYPE_LOOP);
 
-	startGame = initMassageBox("images/enpitu.png", "ゲーム開始", GetColor(0, 0, 0), Font00, 180, 370, 170, 60);
-	endGame = initMassageBox("images/enpitu.png", "終了", GetColor(0, 0, 0), Font00, 480, 370, 170, 60);
+	startButton[FALSE]= initSprite("images/button/Title_start1.png", BUTTON_W, BUTTON_H);
+	startButton[TRUE] = initSprite("images/button/Title_start2.png", BUTTON_W, BUTTON_H);
+	endButton[FALSE] = initSprite("images/button/Title_end1.png", BUTTON_W, BUTTON_H);
+	endButton[TRUE] = initSprite("images/button/Title_end2.png", BUTTON_W, BUTTON_H);
+
 	startSelect = 0;
 	endSelect = 0;
-	startGame.mystr.alpha = 255 * 0.5;
-	endGame.mystr.alpha = 255 * 0.5;
 }
 
 void Title_Finalize() {
 	DeleteGraph(Title);						//画像の開放処理
-	deleteMassageBox(&startGame);
-	deleteMassageBox(&endGame);
 	deleteSprite(&backSprite);
 	DeleteSoundMem(title_bgm);
 }
@@ -58,50 +62,47 @@ void Title_Update() {
 void Title_Draw() {
 	drawAtSprite(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, &backSprite, TRUE);
 	DrawGraph(50, 50, Title, FALSE);		
-	drawAtMassageBox(&startGame,TRUE);
-	drawAtMassageBox(&endGame, TRUE);
+	for (int i = 0; i < 2; i++) {					//startSelectとendSelectが変わると画像も変わる
+		drawSprite(300, 240 , &startButton[startSelect], TRUE);
+		drawSprite(300, 360, &endButton[endSelect], TRUE);
+	}
 	
 }
 
-
-void Title_StartMouseSelect() {				
-	if (Mousex >= startGame.mystr.x-startGame.sprite.width / 2 + 10&& Mousex <= startGame.mystr.x + startGame.sprite.width / 2 - 30
-		&& Mousey >= startGame.mystr.y - startGame.sprite.height / 2 + 10 && Mousey <= startGame.mystr.y + startGame.sprite.height / 2 - 10) {
-		startGame.mystr.color = 0xff0000;
+										//カーソルを開始に合わせると光る処理
+void Title_StartMouseSelect() {			//xとy手打ちは許してください	
+	if (Mousex >= 300 && Mousex <= 300 + startButton[0].width 
+		&& Mousey >= 240 && Mousey <=240 + startButton[0].height) {
 		if (startSelect == 0) {			//SE管理用のIF文
 			PlaySoundMem(title_select_SE, DX_PLAYTYPE_BACK);
 			startSelect = 1;
 		}
 		if ( getLeftDown() != 0) {
-			startGame.mystr.color = 0x00ff00;
 			PlaySoundMem(title_enter_SE, DX_PLAYTYPE_BACK);
 			onLoadFlag(LOAD_FLUSH);
 		}
 	}
 	else {
-		startGame.mystr.color = 0x000000;
 		startSelect = 0;
 	}
 	
 }
-
-void Title_EndMouseSelect() {
-	if (Mousex >= endGame.mystr.x - endGame.sprite.width / 2 + 10 && Mousex <= endGame.mystr.x + endGame.sprite.width / 2 - 30
-		&& Mousey >= endGame.mystr.y - endGame.sprite.height / 2 + 10 && Mousey <= endGame.mystr.y + endGame.sprite.height / 2 - 10) {
-		endGame.mystr.color = 0xff0000;
+										//カーソルを終了に合わせると光る処理
+void Title_EndMouseSelect() {			//xとy手打ちは許してください
+	if (Mousex >= 300 && Mousex <= 300 + startButton[0].width
+		&& Mousey >= 360 && Mousey <= 360 + startButton[0].height) {
 		if (endSelect == 0) {
 			PlaySoundMem(title_select_SE, DX_PLAYTYPE_BACK);
 			endSelect = 1;
 		}
 		if (getLeftDown() != 0) {
 			PlaySoundMem(title_enter_SE, DX_PLAYTYPE_BACK);
-			endGame.mystr.color = 0x00ff00;
 			/*ここに終了処理を描く*/
+			DxLib_End();
 
 		}
 	}
 	else {
-		endGame.mystr.color = 0x000000;
 		endSelect = 0;
 	}
 }
